1edb42f38c65114e9090ef1301f0ca5b3a10c968
Klimov Paul <klimov.paul@gmail.com>
2017-07-27 15:36:05 +0300

    HHVM removed from Travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,3 +13,5 @@ require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 Yii::setAlias('@yii2tech/tests/unit/ar/eagerjoin', __DIR__);
 Yii::setAlias('@yii2tech/ar/eagerjoin', dirname(__DIR__));
+
+require_once(__DIR__ . '/compatibility.php');
